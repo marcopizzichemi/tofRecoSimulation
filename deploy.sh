7370f19359cd412779dc536eae1bbcaf6b0c1785
marcopizzichemi <marco.pizzichemi@gmail.com>
2019-03-20 16:52:16 +0100

    added more parameters to macro

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,16 @@
 #|/bin/bash
-
-g++ -o ./bin/tofReco ./scripts/tofReco.cpp `root-config --cflags --libs`
+if [ -z "$1" ]
+  then
+    echo "No argument supplied, so compiling with machine compiler..."
+    g++ -o ./bin/tofReco ./scripts/tofReco.cpp `root-config --cflags --libs`
+else
+  if [ $1 = "lxplus" ]; then
+    echo "Sourcing compiler, ROOT and Geant4 for lxplus..."
+    source /cvmfs/sft.cern.ch/lcg/app/releases/ROOT/6.06.08/x86_64-slc6-gcc49-opt/root/bin/thisroot.sh
+    source /cvmfs/sft.cern.ch/lcg/external/gcc/4.9.1/x86_64-slc6/setup.sh
+    source /cvmfs/geant4.cern.ch/geant4/10.3/x86_64-slc6-gcc49-opt/bin/geant4.sh
+    g++ -o ./bin/tofReco ./scripts/tofReco.cpp `root-config --cflags --libs`
+  else
+    echo "Invalid argument $1"
+  fi
+fi
